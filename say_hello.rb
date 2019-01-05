9c78073c69b8c48d6394ceee1aeba728d6c078c3
Anthony Hillen <ah205@students.uwf.edu>
2019-01-05 06:08:15 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,10 +1,7 @@
 # Build your say_hello method here
-def say_hello(name)
-  if name != nil
-    puts "Hello #{name}!"
-  else
-    puts "Hello Ruby Programmer!"
+def say_hello(name = "Ruby Programmer")
+  puts "Hello #{name}!"
 end
 
 say_hello("Kent Beck")
-say_hello()
+
